4dcf969c23bcff81a0a4f2d5061e4200c311bfd6
temiloluwa <temireply@gmail.com>
2023-04-05 12:08:59 +0200

    initialize repo

---
+++ nlp-dev-base/configure-env.sh
@@ -0,0 +1,23 @@
+# default arguments
+sshKey=macbook-ssh-private-key
+sshRoot="/root/.ssh"
+sshKeyPath="/root/.ssh/id_rsa"
+awsProfile=tobi
+
+# install awscli
+apt-get update --allow-releaseinfo-change && apt-get install -y apt-transport-https
+apt-get install -y curl unzip groff less wget
+apt install make -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip && ./aws/install
+
+# aws configure an aws profile - get credentials from parameter store
+aws configure --profile $awsProfile
+aws configure set aws_access_key_id $keyId
+aws configure set aws_secret_access_key $keyValue
+aws configure set default.region us-east-1
+
+# get ssh credentials from secrets manager
+mkdir -p $sshRoot
+aws secretsmanager get-secret-value --secret-id $sshKey --query SecretString --output text > $sshKeyPath
+chmod 400 $sshKeyPath
